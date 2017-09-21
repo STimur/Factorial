f83343cb76a02fe7815d0a2f35447f2fcb457075
Timur <timur.salakhetdinov@gmail.com>
2017-09-21 04:36:12 +0300

    clean code

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,12 +1,10 @@
 public class Factorial {
     public static int of(int n) {
-        if (n < 2)
-            return 1;
         return of(n, 1);
     }
 
     private static int of(int n, int acc) {
-        if (n == 1)
+        if (n < 2)
             return acc;
         return of(n - 1, acc * n);
     }
