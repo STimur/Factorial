98af84f6986bd2ce0ed765cbe5135d71d8a4e418
Timur <timur.salakhetdinov@gmail.com>
2017-09-21 04:34:45 +0300

    refactor to tail-recursion

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -2,6 +2,12 @@ public class Factorial {
     public static int of(int n) {
         if (n < 2)
             return 1;
-        return n * of(n - 1);
+        return of(n, 1);
+    }
+
+    private static int of(int n, int acc) {
+        if (n == 1)
+            return acc;
+        return of(n - 1, acc * n);
     }
 }
